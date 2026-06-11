4d270fc11ce95143c8e1b200401a429b23a9c4f3
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-06-11 05:30:16 -0300

    clean5

---
+++ l1/l1.ts
@@ -0,0 +1,3 @@
+/// <mls fileReference="_102045_/l1/l1.ts" enhancement="_blank"/>
+
+console.log('hi')
---
+++ l2/l2.ts
@@ -0,0 +1,3 @@
+/// <mls fileReference="_102045_/l2/l2.ts" enhancement="_blank"/>
+
+console.log("hi")
